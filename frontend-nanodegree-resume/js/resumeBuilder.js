9052f4e17a0f05c30aece064ead5acdb1a5d29c7
Jun Yan <junyan@Juns-Air.attlocal.net>
2016-07-25 10:20:08 -0700

    mv portfolio site to a new repo

--- frontend-nanodegree-resume/js/resumeBuilder.js
+++ frontend-nanodegree-resume/js/resumeBuilder.js
@@ -11,14 +11,45 @@ var bio = {
   },
   "welcomeMessage": "Welcome to my site !",
   "skills": ["python", "html"],
-  "biopic": "images/me.jpeg",
+  "biopic": "images/me.jpeg"
 };
 
 var work = {
   "jobs": [
     {
       "employer": "AT&T",
+      "title": "Data Scientist",
+      "location": "Palo Alto, CA",
+      "dates": "2014-now",
+      "description": "Utilize and understand big data in AT&T to drive business decisions."
+    },
+    {
+      "employer": "Colorado School of Mines",
+      "title": "Research Assistant Professor",
+      "location": "Golden, CO",
+      "dates": "2014",
+      "description": "Research on machine learning methods for material science applications."
+    }
+  ]
+};
 
+var projets = {
+  "projects": [
+    {
+      "title": "Onion",
+      "dates": "2015-2016",
+      "descripton": "Connect people and projects by mining internal organizational data.",
+      "images": [
+        "images/fry.jpg"
+      ]
+    },
+    {
+      "title": "Social Graph Analysis",
+      "dates": "2015",
+      "descripton": "Detect influencers and communities from customer social graph.",
+      "images": [
+        "images/fry.jpg"
+      ]
     }
   ]
 };
@@ -29,15 +60,17 @@ var education = {
       "name": "Stanford",
       "location": "Stanford, CA",
       "degree": "Postdoc",
-      "major": "Chemical Engineering",
-      "years": "2011-2014"
+      "major": ["Chemical Engineering"],
+      "dates": "2014",
+      "url": "www.stanford.edu"
     },
     {
       "name": "DTU",
       "location": "Lyngby, Denmark",
       "degree": "Postdoc",
-      "major": "Physics",
-      "years": "2009-2011"
+      "major": ["Physics"],
+      "dates": "2011",
+      "url": "www.dtu.dk"
     }
   ],
   "onlineCourses": [
@@ -45,7 +78,7 @@ var education = {
       "title": "Udacity web developer nanodegree",
       "school": "Udacity",
       "dates": 2016,
-      "url": "https:www.udacity.com"
+      "url": "www.udacity.com"
     }
   ]
 };
@@ -57,9 +90,9 @@ var education = {
 // $("#topContacts").append(HTMLmobile.replace("%data%", bio.mobile));
 // $("#topContacts").append(HTMLemail.replace("%data%", bio.email));
 
-$("#workExperience").append(HTMLworkStart);
-$("#workExperience").append(HTMLworkEmployer.replace("%data%", work["employer"]));
-$("#workExperience").append(HTMLworkTitle.replace("%data%", work["position"]));
-$("#education").append(HTMLschoolStart);
-$("#education").append(HTMLschoolName.replace("%data%", education.name))
-$("#education").append(HTMLschoolDegree.replace("%data%", education.degree))
+// $("#workExperience").append(HTMLworkStart);
+// $("#workExperience").append(HTMLworkEmployer.replace("%data%", work["employer"]));
+// $("#workExperience").append(HTMLworkTitle.replace("%data%", work["position"]));
+// $("#education").append(HTMLschoolStart);
+// $("#education").append(HTMLschoolName.replace("%data%", education.name))
+// $("#education").append(HTMLschoolDegree.replace("%data%", education.degree))
