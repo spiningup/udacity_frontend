c0482e2001e46bfa0cad36f59da44d6be2e2b615
Jun Yan <junyan@Juns-Air.attlocal.net>
2016-09-03 19:40:24 -0700

    catclicker premium pro

--- catclicker/script.js
+++ catclicker/script.js
@@ -7,7 +7,10 @@ $(function(){
             'clickcount': 0},
            {'name': 'cat2',
             'imgsrc': 'https://lh3.ggpht.com/nlI91wYNCrjjNy5f-S3CmVehIBM4cprx-JFWOztLk7vFlhYuFR6YnxcT446AvxYg4Ab7M1Fy0twaOCWYcUk=s0#w=640&h=426',
-            'clickcount': 0}]
+            'clickcount': 0}],
+    adminInfo: {'name': null,
+                'imgsrc': null,
+                'clickcount': null}
   };
 
   var octopus = {
@@ -15,6 +18,7 @@ $(function(){
       model.currentCat = model.cats[0];
       viewList.init();
       viewCat.init();
+      viewAdmin.init();
     },
 
     getCurrentCat: function() {
@@ -32,6 +36,22 @@ $(function(){
     addCounter: function() {
       model.currentCat.clickcount++;
       viewCat.render();
+    },
+
+    setCurrentCatAdmin: function() {
+      model.adminInfo.name = model.currentCat.name;
+      model.adminInfo.imgsrc = model.currentCat.imgsrc;
+      model.adminInfo.clickcount = model.currentCat.clickcount;
+      viewAdmin.render();
+    },
+
+    saveCurrentCatAdmin: function() {
+      model.currentCat.name = $("#input-name").val();
+      model.currentCat.imgsrc = $("#input-url").val();
+      model.currentCat.clickcount = parseInt($('#input-clicks').val());
+      viewList.render();
+      viewCat.render();
+      this.setCurrentCatAdmin();
     }
   };
 
@@ -41,12 +61,14 @@ $(function(){
     },
 
     render: function() {
+      $("#cat-list").empty();
       var cats = octopus.getCats();
       cats.forEach(function(cat, i) {
         $("#cat-list").append('<li id="cat' + i + '">' + cat.name + '</li>');
         $("#cat"+i).click((function(cat) {
           return function() {
             octopus.setCurrentCat(cat);
+            octopus.setCurrentCatAdmin();
             viewCat.render();
           };
         })(cat));
@@ -70,5 +92,32 @@ $(function(){
     }
   };
 
+  var viewAdmin = {
+    init: function() {
+      $("#btn-admin").click(function(e) {
+        octopus.setCurrentCatAdmin();
+      });
+
+      $("#btn-save").click(function(e) {
+        octopus.saveCurrentCatAdmin();
+        e.preventDefault();
+      });
+
+      $("#btn-cancel").click(function(e) {
+        $("#admin-form").hide();
+        e.preventDefault();
+      });
+    },
+
+    render: function() {
+      $("#input-name").val(model.adminInfo.name);
+      $("#input-url").val(model.adminInfo.imgsrc);
+      $("#input-clicks").val(model.adminInfo.clickcount);
+      $("#admin-form").show();
+    }
+
+  };
+  
   octopus.init();
+
 });
